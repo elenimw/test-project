d4c5c028f2c58f368d9ec810417f5e9835fce042
Eleni <elenimulugetaw@gmail.com>
2023-03-18 11:15:25 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>This is my first React app </h1>
-        
-      </header>
+      <h1>THis is my first react app</h1>
     </div>
   );
 }
